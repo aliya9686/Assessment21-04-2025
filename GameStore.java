cee4bb7f26aab868a803f230b5df5e52e8babeab
aliya9686 <aliyamohammadi043@gmail.com>
2025-04-21 18:36:48 +0530

    Add files via upload
    Game Store

---
+++ GameStore.java
@@ -0,0 +1,24 @@
+package Assessment;
+
+import java.util.Scanner;
+
+public class GameStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		System.out.println("Choose Games");
+		System.out.println("1. Call of Warfare   - ₹1500   \r\n"
+				+ "2. Speed Racers      - ₹1200   \r\n"
+				+ "3. Mystery Mansion   - ₹1000   \r\n"
+				+ "4. Pixel Adventure   - ₹800   \r\n"
+				+ "5. Puzzle Mania      - ₹500   \r\n"
+				+ "6. Checkout / Exit");
+		
+		
+
+	}
+
+}
